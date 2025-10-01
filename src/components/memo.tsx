92d33676e87dc5113a9e5b349d4f0ba61da5ba62
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2025-10-01 20:13:34 +0900

    ✨: feat: メモ機能の追加

--- src/components/memo.tsx
+++ src/components/memo.tsx
@@ -1,7 +1,13 @@
+import { Textarea } from "./ui/textarea";
+
 export const Memo = () => {
   return (
     <div className="aspect-square h-56 bg-amber-200">
-      <p>メモ欄</p>
+      <div className="flex justify-between mr-auto mb-1">
+        <p className="text-4xl pl-6 rotate-35">;)</p>
+        <p className="text-lg font-bold pr-2">Memo</p>
+      </div>
+      <Textarea placeholder="メモを入力..." className="h-43 w-56" />
     </div>
   );
 };
