6ff812dac060f79b1a0ffdd118431b2ac589d554
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2025-10-06 19:55:35 +0900

    :gear: chore: github pagesにデプロイする設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "REPOSITORY_NAME" : "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
